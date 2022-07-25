2c7030907b52b81b115b420e1219e84387eb48ff
Adam Tweedie <adam_tweedie@msn.com>
2022-07-25 21:59:58 +0100

    Updates project directory structure and app entry point

---
+++ src/components/app/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Map from '../map/Map'
+
+const App = () => (
+	<div className="App">
+		<Map></Map>
+ 	</div>
+);
+
+export default App;
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,5 @@
+const Map = () => (
+    <h1>MAP</h1>
+)
+
+export default Map
